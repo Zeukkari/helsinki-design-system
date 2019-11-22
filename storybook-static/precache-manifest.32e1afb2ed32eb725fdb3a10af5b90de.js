56e1c14455e2188165adb3c394ca7db511646c99
Timo Aho <zeukkari@gmail.com>
2019-11-22 19:08:01 +0200

    Updates

---
+++ storybook-static/precache-manifest.32e1afb2ed32eb725fdb3a10af5b90de.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587c8f68a388e80cbeac241d8d91c01e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.ae9877aa1a6274431b63.bundle.js"
+  },
+  {
+    "revision": "9fe818d9374b7be76f60",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.ae9877aa1a6274431b63.bundle.js"
+  },
+  {
+    "revision": "a73385e3af548d10aae3406c40374c84",
+    "url": "static/media/placeholder_1400x1164.a73385e3.jpg"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "d828e0e6832531bd82af0376cb715adb",
+    "url": "static/media/placeholder_2_1920x1080.d828e0e6.jpg"
+  },
+  {
+    "url": "vendors~main.ae9877aa1a6274431b63.bundle.js"
+  }
+]);
