27edf14f61cd94d9369811fbe39e5183c5019988
Zeukkari <Zeukkari@users.noreply.github.com>
2019-11-25 06:02:17 +0000

    Automated deployment: Mon Nov 25 06:02:17 UTC 2019 4934c233b729427dd6f375fbf88f83549217f897

---
+++ storybook/precache-manifest.644ad4a73ffe1784bbb39ac30e2a5c84.js
@@ -0,0 +1,31 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ab60c15040cf65bad2bd970819c35f",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "7cba2097f150d5b2a606",
+    "url": "main.css"
+  },
+  {
+    "url": "main.e20c5d9b848ab68e4601.bundle.js"
+  },
+  {
+    "url": "runtime~main.e20c5d9b848ab68e4601.bundle.js"
+  },
+  {
+    "revision": "a73385e3af548d10aae3406c40374c84",
+    "url": "static/media/placeholder_1400x1164.a73385e3.jpg"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "revision": "d828e0e6832531bd82af0376cb715adb",
+    "url": "static/media/placeholder_2_1920x1080.d828e0e6.jpg"
+  },
+  {
+    "url": "vendors~main.e20c5d9b848ab68e4601.bundle.js"
+  }
+]);
--- storybook/service-worker.js
+++ storybook/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.76b0c6bbeee2883894bfbea99c1d3733.js"
+  "precache-manifest.644ad4a73ffe1784bbb39ac30e2a5c84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
